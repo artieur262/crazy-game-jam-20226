859952813e54bcd5cb105fb142392447cfb32cee
CHANUT--ALBERT Giovanni <chanutalbert.giovanni@outlook.fr>
2026-07-03 23:12:55 +0200

    Renomage scene principale -> scene chariot

--- scenes/chariot/chariot.gd
+++ scenes/chariot/chariot.gd
@@ -1,11 +1,22 @@
 extends VSplitContainer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Corrige la position du Node2D (qui bouge au lancement pour une raison inconue)
 	$MapSubViewportContainer/MapSubViewport/Map.position = Vector2.ZERO
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_direction_pressed() -> void:
+	pass
+
+
+func _on_reparer_pressed() -> void:
+	pass
+
+
+func _on_explorer_pressed() -> void:
+	pass
+
+
+func _on_préparer_un_camp_pressed() -> void:
 	pass
