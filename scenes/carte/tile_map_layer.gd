f9c4b557588998b96ed21130ba8eb686c2250927
Artieur262 <raguinmateo@gmail.com>
2026-07-03 21:50:42 +0200

    correction

--- scenes/carte/tile_map_layer.gd
+++ scenes/carte/tile_map_layer.gd
@@ -13,3 +13,11 @@ func _process(delta: float) -> void:
 		for j in range(3):
 			erase_cell(Vector2i(-1+i,-5+j))
 	
+
+
+func decouvrir_autour(pos_p:Vector2):
+	var position_grill := map_to_local(pos_p)
+	for i in range(rayon_de_decouverte):
+		for j in range(rayon_de_decouverte):
+			erase_cell(Vector2i(position_grill.x+i, position_grill.y+j))
+	
