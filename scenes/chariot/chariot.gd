791f535917af1b8749291fbad0b4e6c2f8c07c0d
CHANUT--ALBERT Giovanni <chanutalbert.giovanni@outlook.fr>
2026-07-04 00:58:36 +0200

    Ajout de documentation

--- scenes/chariot/chariot.gd
+++ scenes/chariot/chariot.gd
@@ -6,17 +6,21 @@ func _ready() -> void:
 	$MapSubViewportContainer/MapSubViewport/Map.position = Vector2.ZERO
 
 
+## Appellé quand le bouton "choisir une direction" est appuyé.
 func _on_direction_pressed() -> void:
 	pass
 
 
+## Appellé quand le bouton "réparer le chariot" est appuyé.
 func _on_reparer_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/reparations/reparations.tscn")
 
 
+## Appellé quand le bouton "explorer" est appuyé.
 func _on_explorer_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/explorations/explorations.tscn")
 
 
+## Appellé quand le bouton "préparer le camp" est appuyé.
 func _on_preparer_un_camp_pressed() -> void:
 	pass
