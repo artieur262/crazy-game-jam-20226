d2cf1295ef49960b1a723c1ebb69b5364ce512bd
badis <badis.saad@etu.umontpellier.fr>
2026-07-03 23:39:40 +0200

    systeme de noeud et arbre

--- scenes/noeud/noeud.gd
+++ scenes/noeud/noeud.gd
@@ -1,13 +1,17 @@
-extends Node
-
-var noeuds : Array = []
-var chemins : Array = []
-
-func reset():
-	noeuds.clear()
-	chemins.clear()
+extends Node2D
 
 
+@export var id : int = 0
+var voisins : Array = []
+
+func ajouter_voisin(n):
+	if n == self:
+		return
+	if n in voisins:
+		return
+		
+	voisins.append(n)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
