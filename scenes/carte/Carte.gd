f82a3b7b5bdbcf8a30694cece0f30a92be356466
CHANUT--ALBERT Giovanni <chanutalbert.giovanni@outlook.fr>
2026-07-04 20:21:15 +0200

    Modification de la carte

--- scenes/carte/Carte.gd
+++ scenes/carte/Carte.gd
@@ -2,6 +2,10 @@
 extends TileMapLayer
 class_name Carte
 
+## Signal emit par les checkpoint lorsqu'un d'entre eux est cliqué.
+## Voir [signal Checkpoint.selectionne]
+signal checkpoint_selectionne(Checkpoint)
+
 ## Rayon (en nombre de case) autour du personnages qui seront dévoillé lors des déplacements.
 @export var rayon_de_decouverte := 4
 ## Distance max (en pixels) entre deux [Checkpoint]
@@ -10,19 +14,49 @@ class_name Carte
 ## Liste des [Checkpoints] de cette [Carte].
 @export var checkpoints: Array[Checkpoint] = []
 
+## Taile de la carte en nombre de case.
 @export var taille_carte := Vector2i(100, 75)
+## [Checkpoint] de départ de la carte.
+@export var depart: Checkpoint
+## [Checkpoint] d'arrivée de la carte.
+@export var arrive: Checkpoint
 
+## [Camera2D] utilisée sur cette map.
 @onready var camera := $Camera2D
 
 ## Flag déterminant si un drag and drop est en cours.
 var dragging := false
 
-func _ready() -> void:
-	# Jeu.position_joueur.connect(mettre_a_jour)
-	Generateur.genere_routes(self)
+func config():
+	Jeu.checkpoint_depart = depart
+	Jeu.checkpoint_arrive = arrive
+
+func reset_brouillard() -> void:
+	Jeu.joueur_change_de_position.connect(decouvrir_autour)
 	for x in range(taille_carte.x):
 		for y in range(taille_carte.y):
 			set_cell(Vector2i(x, y), 1, Vector2i(0,0), 0)
+
+
+## Reposition les [Checkpoint] pour qu'il soit centré sur les tuile.
+func reposition_checkpoints():
+	for checkpoint in checkpoints:
+		checkpoint.position = map_to_local(
+			local_to_map(checkpoint.position))
+
+## Returne le [Checkpoint] qui est sur cette tuile.
+## [null] si introuvable.
+func checkpointViaTuile(pos: Vector2i) -> Checkpoint:
+	for checkpoint in checkpoints:
+		if local_to_map(checkpoint.position) == pos:
+			return checkpoint
+	return null
+
+## Returne le [Checkpoint] qui est sur la tuile au coordonées donnée..
+## [null] si introuvable.
+func checkpointViaPos(pos: Vector2) -> Checkpoint:
+	return checkpointViaTuile(local_to_map(pos))
+
 
 
 ## Connecté à [signal Jeu.position_joueur] pour mettre à jour la carte en
@@ -39,15 +73,16 @@ func decouvrir_autour(pos_globale: Vector2):
 	decouvrir_autour_de_case(position_grille)
 
 
-## Prend des coordonées de la case et dévoiles les cases avec pour rayon [member rayon_de_decouverte].
-## Voir [method decouvrir_autour] pour utiliser des coordonées globales/locales au lieu d'une coordonée
+## Prend des coordonées de la case et dévoiles les cases avec pour rayon
+## [member rayon_de_decouverte].  Voir [method decouvrir_autour] pour
+## utiliser des coordonées globales/locales au lieu d'une coordonée
 ## de tuile.
 func decouvrir_autour_de_case(position_grille: Vector2i):
 	for i in range(-rayon_de_decouverte+1, rayon_de_decouverte):
 		for j in range(-rayon_de_decouverte+1,rayon_de_decouverte):
 			erase_cell(Vector2i(position_grille.x+i, position_grille.y+j))
-	
-	
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouse:
 		if event.is_action("drag"):
--- scenes/carte/Checkpoint.gd
+++ scenes/carte/Checkpoint.gd
@@ -3,11 +3,37 @@
 extends Node2D
 class_name Checkpoint
 
+## Émit quand ce checkpoint est selectionné.
+## Pour écouter indépendamment du checkpoint voir [signal Map.checkpoint_selectionne]
+signal selectionne
+
+## [Carte] associé à ce checkpoint.
+@onready var carte: Carte = get_parent()
 ## Liste des [Checkpoint] auquel celui-ci est connecté
 @export var connections: Array
+## Définir si le checkpoint a été visité par le joueur.
+var visite := false
+var mesh: MeshInstance2D
+
+const DESSUS := preload("res://assets/carte/checkpoint visite.png")
+const PAS_DESSUS := preload("res://assets/carte/checkpoint.png")
+
+
 
 func _init():
+	visite = false
 	connections = []
+
+
+func joueur_dessus(toogle: bool):
+	if mesh == null:
+		for child in get_children():
+			if child is MeshInstance2D:
+				mesh = child
+				break
+		if mesh == null:
+			return
+	mesh.texture = DESSUS if toogle else PAS_DESSUS
 
 ## Ajoute le [Checkpoint] donnée à la liste des connection si il n'est pas déjà connecté.
 func connecte_checkpoint(checkpoint: Checkpoint) -> void:
@@ -30,3 +56,17 @@ func liste_connection() -> Array[Checkpoint]:
 ## Return un [bool] indiquant si le checkpoint donnée est connecté.
 func est_connecte(checkpoint: Checkpoint) -> bool:
 	return connections.has(checkpoint)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("selectionner"):
+		var mouse_pos := get_local_mouse_position()
+		var rayon: Vector2 = Vector2.ONE * (5/carte.camera.zoom.x)
+		if (
+				mouse_pos.x > -rayon.x and
+				mouse_pos.y > -rayon.y and
+				mouse_pos.x < rayon.x and
+				mouse_pos.y < rayon.y):
+			carte.checkpoint_selectionne.emit(self)
+			selectionne.emit(self)
+			get_viewport().set_input_as_handled()
+		pass
