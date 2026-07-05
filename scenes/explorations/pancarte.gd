e28f04edb3e02e631bb4531891bb1aededa80940
Clément Vidal <vidal.clement42@proton.me>
2026-07-05 11:13:21 +0200

    pancarte finie

--- scenes/explorations/pancarte.gd
+++ scenes/explorations/pancarte.gd
@@ -2,24 +2,37 @@ extends StaticBody2D
 
 @onready var area_2d: Area2D = $Area2D
 @onready var label: Label = $Label
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var pancarte: StaticBody2D = $"."
 
 var is_inside = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	label.visible = false
+	sprite_2d.texture = preload("res://assets/exploration/changing/Pancarte.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if(is_inside):
+		sprite_2d.texture = preload("res://assets/exploration/changing/pancarte_selected.png")
+	else:
+		sprite_2d.texture = preload("res://assets/exploration/changing/Pancarte.png")
 	if(Input.is_action_just_pressed("interact") and is_inside):
 		print("retour au chariot")
 		Jeu.retour_chariot()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	label.visible = true
-	is_inside = true
+	if body == pancarte:
+		pass
+	else :
+		label.visible = true
+		is_inside = true
 	
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	label.visible = false
-	is_inside = false
+	if body == pancarte:
+		pass
+	else :
+		label.visible = false
+		is_inside = false
