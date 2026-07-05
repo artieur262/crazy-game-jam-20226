9ad846e836df6a77a016334b4fedce9c7b55321e
CHANUT--ALBERT Giovanni <chanutalbert.giovanni@outlook.fr>
2026-07-05 08:42:07 +0200

    Ajout du support une seule réparation, corrections bugs sur réparations

--- scenes/reparations/reparations.gd
+++ scenes/reparations/reparations.gd
@@ -1,21 +1,19 @@
 extends VBoxContainer
 
 var domages_initialises: Dictionary[String, RichTextLabel]
+var dommage_selectionne: Dommage
 
 func _ready():
 	$Window.visible=false
 	domages_initialises = {}
-	var cat = Dommage.new()
-	cat.nom="miaou !"
-	cat.objets_necessaires[Items.bois] = 1
-	Jeu.dommages.append(cat)
 	for dommage in Jeu.dommages:
 		ajouter_domages(dommage)
+	afficher_inventaire()
 
 func ajouter_domages(dommage: Dommage):
 	if domages_initialises.has(dommage.id):
 		return
-	var noeud := RichTextLabel.new()
+	var noeud := ReparationItem.new()
 	noeud.text += dommage.nom+ "\n"
 	noeud.text += "Nécessite:\n"
 	noeud.bbcode_enabled = true
@@ -27,12 +25,22 @@ func ajouter_domages(dommage: Dommage):
 			objet.nom, 
 			Jeu.inventaire.quantite(objet),
 			dommage.objets_necessaires[objet]])
+	for objet in dommage.outils_necessaires:
+		var couleur : String ="%s"
+		if Jeu.inventaire.quantite(objet)==0:
+			couleur="[color=red]%s[/color]\n" 
+		noeud.text +=couleur % ("-%s (%d/%d)\n" % [
+			objet.nom, 
+			Jeu.inventaire.quantite(objet),
+			1])
 	noeud.text = noeud.text.left(-1)
 	domages_initialises[dommage.id] = noeud
 	noeud.size_flags_horizontal=Control.SIZE_FILL
 	noeud.scroll_active=false
 	noeud.fit_content=true
 	$"Main/Réparations/B".add_child(noeud)
+	noeud.reparer.connect(_on_reparer)
+	noeud.dommage = dommage
 
 func supprimer_dommages(dommage: Dommage):
 	if not domages_initialises.has(dommage.id):
@@ -40,50 +48,70 @@ func supprimer_dommages(dommage: Dommage):
 	var noeud := domages_initialises[dommage.id]
 	$"Main/Réparations/B".remove_child(noeud)
 
-func ajouter_item(item: Item):
-	pass
-
 
 func afficher_inventaire():
 	var noeud : RichTextLabel
 	for objet in Jeu.inventaire:
 		noeud = RichTextLabel.new()
 		noeud.bbcode_enabled = true
-		noeud.text +="%d" % Jeu.inventaire.quantite(objet)
+		noeud.text += "%s\n%d" % [objet.nom, Jeu.inventaire.quantite(objet)]
 		noeud.size_flags_horizontal=Control.SIZE_EXPAND_FILL
-		$Inventaires/HBoxContainer.add_child(noeud)
+		noeud.fit_content = true
+		$I/Inventaire.add_child(noeud)
 
 
-func _on_button_button_down() -> void:
-	if verificateur_peut_reparer():
+func _on_reparer(item: ReparationItem) -> void:
+	$"Window/mini-jeu-1".stop()
+	$Window.hide()
+	dommage_selectionne = null
+	if verificateur_peut_reparer(item.dommage):
+		dommage_selectionne = item.dommage
 		$"Window/mini-jeu-1".start()
 		$Window.visible=true
-
+	else:
+		$Controles/Label.text = "Cliquez sur une des options ci-dessous pour la réparer." + \
+			"Cette réparation est impossible."
+		$Timer.start()
 
 func _on_window_close_requested() -> void:
 	$"Window/mini-jeu-1".stop()
 	$Window.visible=false
 
-func verificateur_peut_reparer()->bool:
-	for dommage in Jeu.dommages:
-		for item in dommage.objets_necessaires:
-			var quantite_requise = dommage.objets_necessaires[item]
-			if quantite_requise < Jeu.inventaire.quantite(item):
-				return false
-		for outil in dommage.outils_necessaires:
-			if Jeu.inventaire.quantite(outils_necessaires) ==0:
-				return false
+func verificateur_peut_reparer(dommage: Dommage) -> bool:
+	for item in dommage.objets_necessaires:
+		var quantite_requise = dommage.objets_necessaires[item]
+		if quantite_requise > Jeu.inventaire.quantite(item):
+			return false
+	for outil in dommage.outils_necessaires:
+		if Jeu.inventaire.quantite(outil) == 0:
+			return false
 	return true
 
 
-func _on_minijeu_1_quitter(boole: Variant) -> void:
-	if boole:
+func _on_minijeu_1_quitter(reussi: Variant) -> void:
+	if reussi:
 		fabriquer()
 	$"Window/mini-jeu-1".stop()
-	$Window.visible=false
+	$Window.hide()
+	dommage_selectionne = null
+
 
 func fabriquer():
-	for dommage in Jeu.dommages:
-		for object in dommage.objets_necessaires:
-			Jeu.inventaire.supprime_item(object,dommage.objets_necessaires[object])
-			
+	for object in dommage_selectionne.objets_necessaires:
+		Jeu.inventaire.supprime_item(object,dommage_selectionne.objets_necessaires[object])
+	supprimer_dommages(dommage_selectionne)
+	Jeu.dommages.erase(dommage_selectionne)
+	for child in $I/Inventaire.get_children():
+		child.queue_free()
+	afficher_inventaire()
+
+
+func _on_timer_timeout() -> void:
+	$Controles/Label.text = "Cliquez sur une des options ci-dessous pour la réparer."
+	
+
+func _on_button_pressed() -> void:
+	$ConfirmationDialog.show()
+
+func _on_confirmation_dialog_confirmed() -> void:
+	Jeu.retour_chariot()
